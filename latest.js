3254553dd47ea24704bcca5e8f2df7fb637f48f9
aditya <aditya@aditya.com>
2015-05-06 13:29:00 +0530

    today

---
+++ latest.js
@@ -0,0 +1,17 @@
+myFunction2([
+{
+"news": "As a part of the limited loyalty programme, existing BlackBerry users can get up to Rs. 10,000 discount while buying select BlackBerry 10 smartphones - the BlackBerry Passport, BlackBerry Z30, BlackBerry Z3 and the BlackBerry Classic."
+},
+{
+"news": "As a part of the limited loyalty programme, existing BlackBerry users can get up to Rs. 10,000 discount while buying select BlackBerry 10 smartphones - the BlackBerry Passport, BlackBerry Z30, BlackBerry Z3 and the BlackBerry Classic."
+},
+{
+"news": "As a part of the limited loyalty programme, existing BlackBerry users can get up to Rs. 10,000 discount while buying select BlackBerry 10 smartphones - the BlackBerry Passport, BlackBerry Z30, BlackBerry Z3 and the BlackBerry Classic."
+},
+{
+"news": "As a part of the limited loyalty programme, existing BlackBerry users can get up to Rs. 10,000 discount while buying select BlackBerry 10 smartphones - the BlackBerry Passport, BlackBerry Z30, BlackBerry Z3 and the BlackBerry Classic."
+},
+{
+"news": "As a part of the limited loyalty programme, existing BlackBerry users can get up to Rs. 10,000 discount while buying select BlackBerry 10 smartphones - the BlackBerry Passport, BlackBerry Z30, BlackBerry Z3 and the BlackBerry Classic."
+}
+])
---
+++ pushnews.js
@@ -0,0 +1,34 @@
+myFunction([
+{
+"image": "https://unsplash.imgix.net/photo-1429371527702-1bfdc0eeea7d?q=75&fm=jpg&w=1080&fit=max&s=4d76bc86dd818a2cf0aa286b3aa0fd9f",
+"head": "Good morning",
+"para": "Earthquake in Nepal, indian political news and much more in today's 2min 54 seconds digest"
+},
+{
+"image": "http://static.ibnlive.in.com/ibnlive/pix/sitepix/11_2012/rajya-sabha.jpg",
+"head": "Bill to operationalize Land Boundary Agreement in Rajya Sabha soon",
+"para": "According to sources, government has taken states like West Bengal, Tripura and Meghalaya on board and the Bill will include land only in these states and will keep Assam out of it due to sensivities in that state where BJP aspires to come to power in assembly polls are due early next year."
+},
+{
+"image": "http://www.aninews.in/contentimages/detail/indiaamericaflag2.jpg",
+"head": "Bill to operationalize Land Boundary Agreement in Rajya Sabha soon",
+"para": "According to sources, government has taken states like West Bengal, Tripura and Meghalaya on board and the Bill will include land only in these states and will keep Assam out of it due to sensivities in that state where BJP aspires to come to power in assembly polls are due early next year."
+},
+{
+"image": "http://s3.firstpost.in/wp-content/uploads/2015/04/Sarfraz-vs-RR-BCCI.jpg",
+"head": "Bill to operationalize Land Boundary Agreement in Rajya Sabha soon",
+"para": "According to sources, government has taken states like West Bengal, Tripura and Meghalaya on board and the Bill will include land only in these states and will keep Assam out of it due to sensivities in that state where BJP aspires to come to power in assembly polls are due early next year."
+},
+{
+"image": "http://stech3.firstpost.com/tech2images/640x359/proportional/jpeg/2015/04/lg-g4-1-624x351.jpg",
+"head": "Bill to operationalize Land Boundary Agreement in Rajya Sabha soon",
+"para": "According to sources, government has taken states like West Bengal, Tripura and Meghalaya on board and the Bill will include land only in these states and will keep Assam out of it due to sensivities in that state where BJP aspires to come to power in assembly polls are due early next year."
+},
+{
+"image": "http://media.giphy.com/media/Edyt9XEXgzejC/giphy.gif",
+"head": "That's all",
+"para": "Have a great day."
+}
+])
+
+
